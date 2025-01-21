ebb672b73853f7a97cbe7bb7ad7abea1353b1268
Zack Siri <me@zacksiri.dev>
2025-01-21 16:11:20 +0000

    configure CUDA

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,3 +4,7 @@ if :os.type() == {:unix, :darwin} do
   config :nx,
     default_backend: {EMLX.Backend, device: :gpu}
 end
+
+if :os.type() == {:unix, :linux} do
+  config :nx, :default_backend, {EXLA.Backend, client: :cuda}
+end
