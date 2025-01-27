3a3d9f144ee5e8b5e4bd707b35cb7cf945e1aa0b
Zack Siri <me@zacksiri.com>
2025-01-27 16:48:07 +0700

    remove unused deps

--- mix.exs
+++ mix.exs
@@ -25,9 +25,7 @@ defmodule Opsmo.MixProject do
       {:nx, "~> 0.9"},
       {:axon, github: "elixir-nx/axon"},
       {:safetensors, "~> 0.1"},
-      {:req, "~> 0.5.0"},
-      {:explorer, "~> 0.10.0"},
-      {:table_rex, "~> 4.0", optional: true, override: true}
+      {:req, "~> 0.5.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
