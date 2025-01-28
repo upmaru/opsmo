ea65635e4cf98f63898a8da2564835879880c39c
Zack Siri <me@zacksiri.com>
2025-01-28 11:04:26 +0700

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Opsmo.MixProject do
   def project do
     [
       app: :opsmo,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.15",
       description: description(),
       license: "Apache-2.0",
